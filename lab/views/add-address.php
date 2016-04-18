d8f6aebac3b380552327b32b7720c74803b94a32
dhankey <djhankey@email.neit.edu>
2016-04-18 14:42:53 -0400

    L1 update

--- lab/views/add-address.php
+++ lab/views/add-address.php
@@ -75,6 +75,12 @@
                 array_push($message,'Sorry zip is not valid');
             }
 
+            if (empty($dob)) 
+            {
+                array_push($message,'Sorry dob is empty');
+            }
+
+
             // Add Address
             if(count($message) === 0)
             {
